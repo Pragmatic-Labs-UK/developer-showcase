af53f2ef7927b0271c20386a254dd2d253a36a52
PragmaticLabs <theo@thinkpragmatic.co.uk>
2026-09-01 16:20:25 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System;
 namespace PragmaticLabs {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("C# Security Grid Active");
+            Console.WriteLine("C# Security Active");
         }
     }
 }
