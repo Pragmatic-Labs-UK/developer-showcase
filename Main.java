0a9a5d204ed20d2a08278bd187356041b2c684fd
PragmaticLabs <theo@thinkpragmatic.co.uk>
2026-09-01 16:20:11 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Enterprise Core Active");
+        System.out.println("Java Core Active");
     }
 }
